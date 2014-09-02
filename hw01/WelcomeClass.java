3a121f06cf45060bc79019e6358ec4ca0cc900a6
tnn217 <tnn217@lehigh.edu>
2014-09-02 22:28:16 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+//////////////////////////////
+//Tony Nikolov
+//Demo 0
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+  public static void main(String[ ] args)   {
+    
+//  print the statement with box around it
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-T--N--N--2--1--7-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("This July, I went with my family to Bulgaria. ");
+    System.out.println("We visited relatives and went to the beach. ");
+    
+    //test/testtest
+  }
+}
